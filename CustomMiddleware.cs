f28d5944a35bd0969504b619b0235cb4841bf05d
carlosVera777 <carlosvera.777@gmail.com>
2022-11-17 21:04:32 -0500

    Config Middleware

---
+++ CustomMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Core.Services;
+using Microsoft.Extensions.Options;
+using System.Runtime.Serialization;
+
+namespace Core
+{
+	public class CustomMiddleware
+	{
+		private RequestDelegate _next;
+
+		public CustomMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task Invoke(HttpContext context, IResponseFormatter formatter1, IResponseFormatter formatter2, IResponseFormatter formatter3)
+		{
+			if (context.Request.Path == "/middleware")
+			{
+				await formatter1.Format(context, String.Empty);
+				await formatter2.Format(context, String.Empty);
+				await formatter3.Format(context, String.Empty);
+			}
+			if (_next != null)
+			{
+				await _next(context);
+			}
+		}
+	}
+}
